ce014022f87f190a92d9781298e757a17307a637
Palpitate <xus1758011080@outlook.com>
2023-06-28 19:50:28 +0800

    更新路由

--- vue-mall_demo/src/router/index.js
+++ vue-mall_demo/src/router/index.js
@@ -1,15 +1,49 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-
+import Products from '../components/Products'
+import Cart from '../components/Cart'
+import PersonalCenter from '../components/PersonalCenter'
+import MallIndex from '../components/MallIndex'
+import OrderManagement from '../components/OrderManagment'
+import AddressManagement from '../components/AddressManagement'
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
+      name: 'Index',
+      component: MallIndex
+    },
+    {
+      path: '/products',
+      name: 'Products',
+      component: Products
+    },
+    {
+      path: '/cart',
+      name: 'Cart',
+      component: Cart
+    },
+    {
+      path: '/personalCenter',
+      name: 'PersonalCenter',
+      component: PersonalCenter
+    },
+    {
+      path: '/orderManagement',
+      name: 'OrderManagement',
+      component: OrderManagement
+    },
+    {
+      path: '/addressManagement',
+      name: 'AddressManagement',
+      component: AddressManagement
+    },
+    // {
+    //   path: '/helloworld',
+    //   name: 'profile',
+    //   component: Profile
+    // }
   ]
 })

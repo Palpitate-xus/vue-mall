9c9e32c0cfae2cef42ff6230c3d71c32fb966b5b
Palpitate <xus1758011080@outlook.com>
2023-06-28 19:51:32 +0800

    修改路由

--- vue-mall_demo/src/App.vue
+++ vue-mall_demo/src/App.vue
@@ -1,13 +1,59 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+      <el-menu-item index="1">首页</el-menu-item>
+      <el-menu-item index="2">商品</el-menu-item>
+      <el-menu-item index="3">购物车</el-menu-item>
+      <el-submenu index="4">
+        <template slot="title">个人中心</template>
+        <el-menu-item index="4-1">个人信息管理</el-menu-item>
+        <el-menu-item index="4-2">订单管理</el-menu-item>
+        <el-menu-item index="4-3">地址管理</el-menu-item>
+        <el-menu-item index="4-4">愿望单</el-menu-item>
+      </el-submenu>
+    </el-menu>
     <router-view/>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  data () {
+    return {
+      activeIndex: '1'
+    }
+  },
+  methods: {
+    handleSelect (key, keyPath) {
+      console.log(key, keyPath)
+      if (key === '1') {
+        this.$router.push('/')
+      }
+      if (key === '2') {
+        this.$router.push('/products')
+      }
+      if (key === '3') {
+        this.$router.push('/cart')
+      }
+      if (key === '4-1') {
+        this.$router.push('/personalCenter')
+        console.log("go to personalCenter");
+      }
+      if (key === '4-2') {
+        this.$router.push('/orderManagement')
+      }
+      if (key === '4-3') {
+        this.$router.push('/addressManagement')
+      }
+      if (key === 7) {
+        this.$router.push('/peopledaily')
+      }
+      if (key === 8) {
+        this.$router.push('/tencent')
+      }
+    }
+  }
 }
 </script>
 
@@ -18,6 +64,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>

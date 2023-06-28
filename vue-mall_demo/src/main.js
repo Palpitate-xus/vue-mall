b530518e628d3eb45d8466b0ddcb2acc77a84b2b
Palpitate <xus1758011080@outlook.com>
2023-06-28 19:51:21 +0800

    引入element ui vuex

--- vue-mall_demo/src/main.js
+++ vue-mall_demo/src/main.js
@@ -1,15 +1,31 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 import router from './router'
+import App from './App.vue';
+// import Vuex from 'vuex';
+import store from './store'
 
+Vue.use(ElementUI);
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
-new Vue({
-  el: '#app',
+// let myvue = new Vue({
+//   el: '#app',
+//   router,
+//   store,
+//   components: { App },
+//   template: '<App/>'
+// })
+
+// console.log(store);
+// console.log(myvue.$store);
+let myvue = new Vue({
+  store,
   router,
-  components: { App },
-  template: '<App/>'
-})
+  render: h => h(App),
+}).$mount('#app')
+
+// console.log(myvue.$store);

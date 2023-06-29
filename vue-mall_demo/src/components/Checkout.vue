c01180dc55033c57d83d8776eaa0c296f6937a5f
Palpitate <xus1758011080@outlook.com>
2023-06-29 11:39:55 +0800

    更新订单结算页面

--- vue-mall_demo/src/components/Checkout.vue
+++ vue-mall_demo/src/components/Checkout.vue
@@ -1,75 +1,110 @@
 <template>
-    <div class="checkout">
-      <h2>订单结算</h2>
-      <div v-if="cartItems.length">
-        <ul>
-          <li v-for="item in cartItems" :key="item.id">
-            {{ item.name }} - {{ item.price }} - Quantity: {{ item.quantity }}
-          </li>
-        </ul>
-        <p>Total Price: {{ totalPrice }}</p>
-        <button @click="placeOrder">提交订单</button>
+  <div class="checkout">
+    <h2>购物车结算</h2>
+    <div v-if="cartItems.length">
+      <el-table :data="cartItems" style="width: 100%">
+        <el-table-column prop="name" label="商品名称"></el-table-column>
+        <el-table-column prop="price" label="商品价格"></el-table-column>
+        <el-table-column prop="quantity" label="购买数量"></el-table-column>
+        <el-table-column label="小计">
+          <template slot-scope="{ row }">
+            {{ calculateSubtotal(row) }}
+          </template>
+        </el-table-column>
+      </el-table>
+      <div class="total">
+        总计: {{ totalPrice }}
       </div>
-      <div v-else>
-        <p>No items in the cart.</p>
+      <div class="actions">
+        <el-button @click="$router.push('./cart')">返回</el-button>
+        <el-button type="primary" @click="placeOrder">提交订单</el-button>
       </div>
     </div>
-  </template>
-  
-  <script>
-  import axios from 'axios';
-  
-  export default {
-    name: 'Checkout',
-    data() {
-      return {
-        cartItems: [],
-      };
+    <div v-else>
+      <p>No items in the cart.</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'Checkout',
+  data() {
+    return {
+      cartItems: [{
+          id: 1,
+          name: 'Product 1',
+          price: 19.99,
+          quantity: 2
+        },
+        {
+          id: 2,
+          name: 'Product 2',
+          price: 29.99,
+          quantity: 1
+        },],
+    };
+  },
+  computed: {
+    totalPrice() {
+      return this.cartItems.reduce(
+        (total, item) => total + item.price * item.quantity,
+        0
+      );
     },
-    computed: {
-      totalPrice() {
-        return this.cartItems.reduce(
-          (total, item) => total + item.price * item.quantity,
-          0
-        );
-      },
+  },
+  created() {
+    this.fetchCartItems();
+  },
+  methods: {
+    fetchCartItems() {
+      axios.get('/api/cart')
+        .then(response => {
+          this.cartItems = response.data;
+        })
+        .catch(error => {
+          console.error('Failed to fetch cart items:', error);
+        });
     },
-    mounted() {
-      this.fetchCartItems();
+    calculateSubtotal(item) {
+      return (item.price * item.quantity).toFixed(2);
     },
-    methods: {
-      fetchCartItems() {
-        axios.get('/api/cart')
-          .then(response => {
-            this.cartItems = response.data;
-          })
-          .catch(error => {
-            console.error('Failed to fetch cart items:', error);
-          });
-      },
-      placeOrder() {
-        // Place order logic
-        // ...
-        // Clear cart after placing the order
-        this.clearCart();
-        alert('Order placed successfully!');
-      },
-      clearCart() {
-        axios.delete('/api/cart')
-          .then(response => {
-            this.cartItems = [];
-          })
-          .catch(error => {
-            console.error('Failed to clear cart:', error);
-          });
-      },
+    async placeOrder() {
+      // Place order logic
+      // ...
+      // Clear cart after placing the order
+      await axios.delete('/api/cart')
+        .then(() => {
+          alert('Order placed successfully!');
+          this.cartItems = [];
+        })
+        .catch(error => {
+          console.error('Failed to place order:', error);
+        });
+      this.$router.push('./payment')
     },
-  };
-  </script>
-  
-  <style>
-  .checkout {
-    margin: 20px;
-  }
-  </style>
-  
+    returnBack() {
+      this.$router.push('./cart')
+    },
+  },
+};
+</script>
+
+<style scoped>
+.checkout {
+  margin: 20px;
+}
+
+.total {
+  margin-top: 20px;
+  text-align: right;
+  font-weight: bold;
+}
+
+.actions {
+  margin-top: 20px;
+  text-align: right;
+}
+</style>

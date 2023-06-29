53323fdc59cf21659375177c2e4a7477126ea0b4
Palpitate <xus1758011080@outlook.com>
2023-06-29 11:40:37 +0800

    添加支付成功页面

---
+++ vue-mall_demo/src/components/PaymentSuccess.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="payment-success">
+        <el-result icon="success" title="支付成功">
+            <template slot="extra">
+                <div class="order-details">
+                    <h3>订单详情</h3>
+                    <p>订单ID: {{ orderId }}</p>
+                    <p>支付金额: {{ paymentAmount }}</p>
+                </div>
+                <el-button type="primary" size="medium">返回</el-button>
+            </template>
+         </el-result>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'PaymentSuccess',
+    computed: {
+      orderId() {
+        // Get order ID from the route or vuex store
+        return this.$route.params.orderId; // Assuming order ID is passed as a route parameter
+        // Alternatively, you can retrieve it from the vuex store
+        // return this.$store.state.order.orderId;
+      },
+      paymentAmount() {
+        // Get payment amount from the route or vuex store
+        return this.$route.params.paymentAmount; // Assuming payment amount is passed as a route parameter
+        // Alternatively, you can retrieve it from the vuex store
+        // return this.$store.state.order.paymentAmount;
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  .payment-success {
+    margin: 20px;
+  }
+  
+  .order-details {
+    margin-top: 20px;
+  }
+  </style>
+  

e09f07ec3f5e26b9e12e61b3720922c1e3b93459
Palpitate <xus1758011080@outlook.com>
2023-06-29 09:31:53 +0800

    添加商品详情对话框

--- vue-mall_demo/src/components/Products.vue
+++ vue-mall_demo/src/components/Products.vue
@@ -14,7 +14,7 @@
                 <el-button
                   type="primary"
                   icon="el-icon-shopping-cart-2"
-                  @click="addToCart(product)"
+                  @click="dialogVisible = true"
                 >
                   加入购物车
                 </el-button>
@@ -25,6 +25,40 @@
         </el-col>
       </el-row>
     </div>
+    <!-- <el-dialog
+      title="加入购物车"
+      :visible.sync="dialogVisible"
+      width="75%"
+      :before-close="handleClose">
+      <span>请选择购买数量</span>
+      <el-form>
+        <el-form-item label="购买数量">
+          <el-input></el-input>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="dialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+      </span>
+    </el-dialog> -->
+    <el-dialog title="商品详情" :visible.sync="dialogVisible" width="45%">
+      <div class="product-details">
+        <div class="product-info">
+          <img :src="product.imageUrl" :alt="product.name" class="product-image" />
+          <div class="product-details-info">
+            <h3>{{ product.name }}</h3>
+            <p>{{ product.description }}</p>
+            <p>Price: ${{ product.price }}</p>
+            <el-input-number v-model="quantity" :min="1" :max="10" :step="1"></el-input-number>
+          </div>
+        </div>
+      </div>
+
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="addToCart">加入购物车</el-button>
+        <el-button @click="dialogVisible = false">返回</el-button>
+      </div>
+    </el-dialog>
   </div>
 </template>
 
@@ -34,6 +68,7 @@ export default {
   name: 'Products',
   data () {
     return {
+      dialogVisible: false,
       products: [
         {
           id: 1,
@@ -48,7 +83,14 @@ export default {
           image: 'https://img12.360buyimg.com/n7/jfs/t1/168650/27/37797/57442/649a977eFb1d43aff/67de18b48ae18900.jpg'
         },
         // Add more products here...
-      ]
+      ],
+      product: {
+        name: 'Product 1',
+        description: 'Product description',
+        price: 19.99,
+        imageUrl: 'https://img12.360buyimg.com/n7/jfs/t1/168650/27/37797/57442/649a977eFb1d43aff/67de18b48ae18900.jpg',
+      },
+      quantity: 1,
     }
   },
   methods: {
@@ -57,7 +99,14 @@ export default {
     console.log(this.$store);
     store.dispatch('cart/addProductToCart', product);
     console.log("add cart success");
-  }
+  },
+  handleClose(done) {
+        this.$confirm('确认关闭？')
+          .then(_ => {
+            done();
+          })
+          .catch(_ => {});
+  },
 }
 }
 </script>

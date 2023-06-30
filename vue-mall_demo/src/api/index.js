d22243da4c89c5e29644b5a8aa23c67ff2f04601
Palpitate <xus1758011080@outlook.com>
2023-06-30 09:44:38 +0800

    修改后端接口为开发环境

--- vue-mall_demo/src/api/index.js
+++ vue-mall_demo/src/api/index.js
@@ -2,13 +2,13 @@ import axios from 'axios';
 
 // 创建 Axios 实例
 const axiosInstance = axios.create({
-  baseURL: 'your-api-base-url',
+  baseURL: 'http://localhost:8000/api/',
 });
 
 // 在请求发送前拦截请求
 axiosInstance.interceptors.request.use(
   config => {
-    // 从本地存储或 Vuex 状态管理中获取 Token
+    // 从本地存储中获取 Token
     const token = localStorage.getItem('token') || '';
     // 将 Token 添加到请求的 Headers 中
     config.headers.Authorization = `Bearer ${token}`;

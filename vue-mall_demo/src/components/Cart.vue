21aa4c9d50e84478f0de9309225a435132d4d01e
Palpitate <xus1758011080@outlook.com>
2023-06-30 14:19:30 +0800

    完成购物车页面前后端连接

--- vue-mall_demo/src/components/Cart.vue
+++ vue-mall_demo/src/components/Cart.vue
@@ -2,14 +2,15 @@
   <div class="cart">
     <h2>购物车</h2>
     <el-table :data="cartItems" style="width: 100%">
-      <el-table-column prop="name" label="商品名称"></el-table-column>
-      <el-table-column prop="price" label="单价"></el-table-column>
-      <el-table-column prop="quantity" label="数量">
+      <el-table-column prop="add_time" label="加入时间"></el-table-column>
+      <el-table-column prop="product_name" label="商品名称"></el-table-column>
+      <el-table-column prop="product_price" label="单价"></el-table-column>
+      <el-table-column prop="(quantity, stock_quantity)" label="数量">
         <template slot-scope="scope">
-          <el-input-number v-model="scope.row.quantity" :min="1" :max="10" @change="updateQuantity(scope.row)"></el-input-number>
+          <el-input-number v-model="scope.row.quantity" :min="1" :max="scope.row.stock_quantity" @change="updateQuantity(scope.row)"></el-input-number>
         </template>
       </el-table-column>
-      <el-table-column label="总价">
+      <el-table-column label="小计">
         <template slot-scope="scope">
           {{ calculateTotal(scope.row) }}
         </template>
@@ -29,24 +30,11 @@
 </template>
   
 <script>
+import axiosInstance from '../api';
 export default {
   data() {
     return {
-      cartItems: [
-        {
-          id: 1,
-          name: 'Product 1',
-          price: 19.99,
-          quantity: 2
-        },
-        {
-          id: 2,
-          name: 'Product 2',
-          price: 29.99,
-          quantity: 1
-        },
-        // Add more cart items here...
-      ]
+      cartItems: [],
     };
   },
   computed: {
@@ -54,21 +42,131 @@ export default {
       return this.cartItems.reduce((total, item) => total + item.quantity, 0);
     },
     totalPrice() {
-      return this.cartItems.reduce((total, item) => total + item.price * item.quantity, 0);
+      return this.cartItems.reduce((total, item) => total + item.product_price * item.quantity, 0).toFixed(2);
     }
   },
+  mounted() {
+    this.getCartList();
+  },
   methods: {
-    updateQuantity(item) {
+    async getCartList() {
+      await axiosInstance.request({
+            method: 'post',
+            url: 'cart/get_user_cart/',
+            data: {}
+          })
+            .then(response => {
+              // 处理响应数据
+              console.log(response);
+              console.log(response.data.data);
+              if(response.data.code == '200')
+              {
+                this.cartItems = response.data.data.shoppingcart;
+                console.log(this.cartItems);
+              }
+              else
+              {
+                this.$message({
+                  message: response.data.message,
+                  type: 'error'
+                })
+              }
+              
+            })
+            .catch(error => {
+              // 处理错误
+              console.error(error);
+            });
+    },
+    async updateQuantity(item) {
+      console.log('this is:', item);
       if (item.quantity < 1) {
         item.quantity = 1;
-      } else if (item.quantity > 10) {
-        item.quantity = 10;
+      } else if (item.quantity > item.stock_quantity) {
+        item.quantity = item.stock_quantity;
       }
+      console.log(item.quantity);
+      await this.removeCart(item);
+      await this.addToCart(item);
     },
     calculateTotal(item) {
-      return (item.price * item.quantity).toFixed(2);
+      return (item.product_price * item.quantity).toFixed(2);
     },
-    removeFromCart(item) {
+    async addToCart(item) {
+      console.log("add cart success");
+      await axiosInstance.request({
+            method: 'post',
+            url: 'cart/add_to_cart/',
+            data: {
+              product_id: item.product_id,
+              quantity: item.quantity,
+            }
+          })
+            .then(response => {
+              // 处理响应数据
+              console.log(response);
+              console.log(response.data.data);
+            })
+            .catch(error => {
+              // 处理错误
+              console.error(error);
+            });
+    },
+    async removeCart(item) {
+      await axiosInstance.request({
+            method: 'post',
+            url: 'cart/remove_from_cart',
+            data: {
+              product_id: item.product_id,
+            }
+          })
+            .then(response => {
+              // 处理响应数据
+              console.log(response);
+              console.log(response.data.data);
+            })
+            .catch(error => {
+              // 处理错误
+              console.error(error);
+            });
+    },
+    async removeFromCart(item) {
+      await axiosInstance.request({
+            method: 'post',
+            url: 'cart/remove_from_cart',
+            data: {
+              product_id: item.product_id,
+            }
+          })
+            .then(response => {
+              // 处理响应数据
+              console.log(response);
+              console.log(response.data.data);
+              // this.$router.push('/');
+              // this.$notify({
+              //   title: '登陆成功',
+              //   message: '欢迎来到在线电子商务平台',
+              //   type: 'success'
+              // });
+              if(response.data.code == '200')
+              {
+                this.getCartList();
+                // this.cartItems = response.data.data.shoppingcart;
+                // console.log(this.cartItems);
+              }
+              else
+              {
+                this.$message({
+                  message: response.data.message,
+                  type: 'error'
+                })
+              }
+              
+            })
+            .catch(error => {
+              // 处理错误
+              console.error(error);
+            });
       const index = this.cartItems.indexOf(item);
       if (index !== -1) {
         this.cartItems.splice(index, 1);
